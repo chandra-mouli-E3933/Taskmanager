6efd099f79badf07d6e64036786dee800c6556e5
chandramouli <chandra.mouli@freshworks.com>
2020-11-05 09:13:12 +0530

    11th commit

--- tests/acceptance/tasks-test.js
+++ tests/acceptance/tasks-test.js
@@ -7,57 +7,94 @@ module('Acceptance | tasks', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('visiting tasks page', async function(assert) {
-    
-    //Tasks 
+  test('Fetching tasks page', async function(assert) {
+
     this.server.createList('task', 5);
     await visit('/tasks');
     assert.equal(currentURL(), '/tasks', 'visited tasks page');
     assert.equal(document.querySelectorAll('a.taskItem').length, 5, 'checking get tasks')
     assert.equal(document.querySelectorAll('button.task-edit').length, 5)
     assert.equal(document.querySelectorAll('button.task-delete').length, 5)
+  });
+      
+    
 
-    //create task
+  test('Adding task', async function(assert) {
+
+    this.server.createList('task', 5);
+    await visit('/tasks');
     await fillIn('input.add-input', 'newtask');
     await click('button.add-input-button');
     assert.equal(document.querySelectorAll("a.taskItem")[document.querySelectorAll("a.taskItem").length-1].textContent, 'newtask ', "Add task works good")
+   });
 
+   test('Editing task', async function(assert) {
 
-    //edit a task 
-    await click(document.querySelectorAll("button.task-edit")[document.querySelectorAll("button.task-edit").length-1]);
-    await visit('/tasks/6');
-    assert.equal(currentURL(), '/tasks/6', 'visited task edit page');
+    this.server.createList('task', 5);
+    await visit('/tasks');
+    await click(document.querySelectorAll("button.task-edit")[0]);
+    await visit('/tasks/1');
+    assert.equal(currentURL(), '/tasks/1', 'visited task edit page');
     await fillIn('input.task-update', 'newtaskupdated');
     await click('button.task-update-button');
-    await click('a.task-update-back-button');
     await visit('/tasks');
-    assert.equal(document.querySelectorAll("a.taskItem")[document.querySelectorAll("a.taskItem").length-1].textContent, 'newtaskupdated ', "Edit task works good")
+    assert.equal(document.querySelectorAll("a.taskItem")[0].textContent, 'newtaskupdated ', "Edit task works good")
+   });
 
-    //delete a task 
+
+
+   test('Deleting task', async function(assert) {
+
+    this.server.createList('task', 6);
+    await visit('/tasks');
     await click(document.querySelectorAll("button.task-delete")[document.querySelectorAll("button.task-delete").length-1])
     assert.equal(document.querySelectorAll('a.taskItem').length, 5, 'Delete task sucessfull')
+   });
 
-    //subtasks
+   test('Fetching subtask', async function(assert) {
+
+    this.server.createList('task', 5);
+    await visit('/tasks');
     await click(document.querySelectorAll("a.taskItem")[0]);
     await visit('/tasks/1/subtasks');
     assert.equal(currentURL(), '/tasks/1/subtasks', 'visited subtask page');
+   });
 
-    //create subtask 
+   test('Adding subtask', async function(assert) {
+
+    this.server.createList('task', 5);
+    await visit('/tasks');
+    await visit('/tasks/1/subtasks');
     await fillIn('input.add-input', 'newsubtask');
     await click('button.add-input-button');
     assert.equal(document.querySelectorAll("a.subtaskItem")[0].textContent, 'newsubtask', "Add subtask works good")
+   });
 
-    //edit a subtask 
+   test('Editing subtask', async function(assert) {
+
+    this.server.createList('task', 5);
+    await visit('/tasks');
+    await visit('/tasks/1/subtasks');
+    await fillIn('input.add-input', 'newsubtask');
+    await click('button.add-input-button');
+
     await click(document.querySelectorAll("button.subtask-edit")[0]);
     await visit('/tasks/1/subtasks/1');
     assert.equal(currentURL(), '/tasks/1/subtasks/1', 'visited subtask edit page');
     await fillIn('input.subtask-update', 'newsubtaskupdated');
     await click('button.subtask-update-button');
-    await click('a.subtask-update-back-button');
     await visit('/tasks/1/subtasks');
     assert.equal(document.querySelectorAll("a.subtaskItem")[0].textContent, 'newsubtaskupdated', "Edit subtask works good")
 
-    //delete subtask 
+   });
+
+   test('Deleting subtask', async function(assert) {
+     
+    this.server.createList('task', 5);
+    await visit('/tasks');
+    await visit('/tasks/1/subtasks');
+    await fillIn('input.add-input', 'newsubtask');
+    await click('button.add-input-button');
     await click(document.querySelectorAll("button.subtask-delete")[0])
     assert.equal(document.querySelectorAll('a.subtaskItem').length, 0, 'Delete task sucessfull')
   });
--- tests/integration/components/add-task-test.js
+++ tests/integration/components/add-task-test.js
@@ -2,31 +2,23 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, fillIn, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-
 module('Integration | Component | add-task', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<AddTask />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
-    // Template block usage:
+  });
+
+  test('Block renders', async function(assert) {
     await render(hbs`
       <AddTask>Add Task</AddTask>
     `);
 
     assert.equal(this.element.textContent.trim(), 'Add Task', 'Yielding works fine');
-
-
-    await render(hbs`
-      <AddTask></AddTask>
-    `);
-    await fillIn('input.add-input', 'newtask');
-    await click('button.add-input-button');
 
   });
 });
